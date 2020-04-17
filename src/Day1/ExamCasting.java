a6b2fc8f2dba775b2855d5ab438e3d3d0002cebc
Gilson <dudrb8922@gmail.com>
2020-04-17 16:41:46 +0900

    1일차-자료형변환예제

---
+++ src/Day1/ExamCasting.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class ExamCasting {
+
+	public static void main(String[] args) {
+		/* 정수형 변수 num에 실수 1.23을 저장하면 0.23이 사라지기 때문에 에러가 방생
+		이를 해결하기 위해 명시적 자료형 변환을 이용하여 1.23을 1로 바꾸어 저장 */
+		int num = (int)1.23;
+		/*실수형 변수 dnum에 정수 1을 저장하면 자동 자료형 변환으로 1.00으로 저장된다.*/
+		double dnum = 1;
+		float fnum = (float)1.23;
+		//float fnum = 1.23f;
+		/*자동 자료형 변환이 되는 경우
+		 *정수 -> 실수
+		 *바이트가 작은 것 -> 큰 것
+		 *float > double
+		 *byte > short > int > long
+		 */
+	}
+
+}
