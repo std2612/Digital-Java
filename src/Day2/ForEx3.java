95aa78ef1e66a01aa8812d30e411b6371c02f6c7
Gilson <dudrb8922@gmail.com>
2020-04-20 17:38:12 +0900

    2일차-For문 예제3

---
+++ src/Day2/ForEx3.java
@@ -0,0 +1,20 @@
+package Day2;
+
+public class ForEx3 {
+
+	public static void main(String[] args) {
+		// 1부터 10까지의 합을 구하는 코드를 for문으로 작성하세요.
+		// 1. 반복 횟수 : i는 1부터 10까지 1씩 증가
+		// 2. 규칙성 :
+		// sum = 0;
+		// i=1, sum = 0 + 1
+		// i=2, sum = 0 + 1 + 2
+		int i, sum = 0;
+		for(i=1; i<=5; i+=1) {
+			sum+=i; // sum = sum + i;
+			System.out.printf("1부터 %d까지의 합 = %d%n", i, sum);
+		}
+
+	}
+
+}
