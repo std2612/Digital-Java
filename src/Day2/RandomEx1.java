edbdfe557c8d2a89b9e4048b5e81405cd50dba5a
Gilson <dudrb8922@gmail.com>
2020-04-20 17:43:55 +0900

    2일차-난수 예제

---
+++ src/Day2/RandomEx1.java
@@ -0,0 +1,20 @@
+package Day2;
+
+public class RandomEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int min = 0;
+		int max = 10;
+		int random = 0;
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+		random = (int)(Math.random()*(max-min+1))+min;
+		System.out.println(random);
+	}
+
+}
