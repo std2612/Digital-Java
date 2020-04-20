b5cb5d25e751f09ddf68f787175a2956ec4f06eb
Gilson <dudrb8922@gmail.com>
2020-04-20 22:33:56 +0900

    2일차-Switch문 예제1

---
+++ src/Day2/SwitchEx1.java
@@ -0,0 +1,25 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class SwitchEx1 {
+
+	public static void main(String[] args) {
+		//switch문을 이용한 홀짝 판별 예제
+		int num = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 : ");
+		num =scan.nextInt();
+		scan.close();
+		switch(num % 2) {
+		case 0:
+			System.out.println(num + "은(는) 짝수입니다.");
+			break;
+		case 1://default:
+			System.out.println(num + "은(는) 홀수입니다.");
+			break;
+		}
+
+	}
+
+}
