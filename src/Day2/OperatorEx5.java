14c14f8c2a88afb30e56076dbeb5e4d6431bca45
Gilson <dudrb8922@gmail.com>
2020-04-20 17:41:02 +0900

    2일차-연산자 예제5

---
+++ src/Day2/OperatorEx5.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+		// 조건 연산자 예제
+		int num = 4;
+		boolean isEven = (num % 2 ==0) ? true : false;
+		boolean isOdd = (num % 2 == 0) ? false : true;
+		System.out.println(num + "는 짝수입니까? : " + isEven);
+		System.out.println(num + "는 홀수입니까? : " + isOdd);
+	}
+
+}
