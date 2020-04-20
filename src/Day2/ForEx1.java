b796d50485aa8729094b129863a2db1a56b0961b
Gilson <dudrb8922@gmail.com>
2020-04-20 17:37:40 +0900

    2일차-For문 예제1

---
+++ src/Day2/ForEx1.java
@@ -0,0 +1,22 @@
+package Day2;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		// for문 기본 예제 : Hello World! 5번 출력
+		int i;
+		for(i=1; i<=5; i++) {
+			System.out.println("Hello World!" + i);
+		}
+		System.out.println("-------------");
+		for(i=0; i<5; i++) {
+				System.out.println("Hello World!" + i);
+		}
+		System.out.println("-------------");
+		//for문 기본 예제 : 1부터 5까지 출력하는 예제
+		for(i=1; i<=5; i++) {
+			System.out.println(i);
+		}		
+	}
+
+}
