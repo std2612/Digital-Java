ecbf1d0782aca5df7156ae662be9999eed80f692
Gilson <dudrb8922@gmail.com>
2020-04-21 13:05:01 +0900

    3일차-DoWhile문 기본 예제

---
+++ src/Day3/DoWhileEx1.java
@@ -0,0 +1,21 @@
+package Day3;
+import java.util.Scanner;
+
+public class DoWhileEx1 {
+
+	public static void main(String[] args) {
+		// do while문 예제
+		int menu=0;
+		Scanner scan = new Scanner(System.in);
+		do{
+			System.out.println("1.메뉴1");
+			System.out.println("2.메뉴2");
+			System.out.println("3.메뉴3");
+			System.out.println("4.종료");
+			System.out.print("메뉴를 입력하세요 : ");
+			menu=scan.nextInt();
+		}while(menu!=4);
+		scan.close();
+	}
+
+}
