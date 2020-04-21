d995edc0e2bab9a4bef2f92551e67779ee224208
Gilson <dudrb8922@gmail.com>
2020-04-21 12:10:25 +0900

    3일차-For문 중첩 예제

---
+++ src/Day3/ForEx9.java
@@ -0,0 +1,21 @@
+package Day3;
+
+public class ForEx9 {
+
+	public static void main(String[] args) {
+		/* *****
+		 * *****
+		 * *****
+		 * *****
+		 * *****
+		 */
+		for(int i=1; i<=5; i+=1) {
+			for(int j=1; j<=5; j+=1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
