9736016a79a9ac2610691c184db3fe4f1d576ae6
Gilson <dudrb8922@gmail.com>
2020-04-21 13:02:59 +0900

    3일차-While문 무한 루프, 기본 예제

---
+++ src/Day3/WhileEx1.java
@@ -0,0 +1,26 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class WhileEx1 {
+
+	public static void main(String[] args) {
+		// while에서 무한 루프 예제
+		/*while(true) {
+			System.out.println("Hi");
+		}
+		*/
+		int menu=0;
+		Scanner scan = new Scanner(System.in);
+		while(menu!=4) {
+			System.out.println("1.메뉴1");
+			System.out.println("2.메뉴2");
+			System.out.println("3.메뉴3");
+			System.out.println("4.종료");
+			System.out.print("메뉴를 입력하세요 : ");
+			menu=scan.nextInt();
+		}
+		scan.close();
+	}
+
+}
