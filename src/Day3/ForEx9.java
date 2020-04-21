85250847d1a2cf1a7ff0f7026ff91fa27ce44e22
Gilson <dudrb8922@gmail.com>
2020-04-21 12:46:30 +0900

    3일차-For문 중첩 예제

--- src/Day3/ForEx9.java
+++ src/Day3/ForEx9.java
@@ -15,7 +15,34 @@ public class ForEx9 {
 			}
 			System.out.println();
 		}
-		
+		/* *
+		 * **
+		 * ***
+		 * ****
+		 * *****
+		 * */
+		for(int i=1; i<=5; i+=1) {
+			for(int j=1; j<=i; j+=1) {
+				System.out.print("*");
+			}
+			System.out.println();			
+		}
+		/*     *
+		 *    **
+		 *   ***
+		 *  ****
+		 * *****
+		 * */
+		for(int i=1; i<=5; i+=1) {
+			for(int j=1; j<=5-i; j+=1) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j+=1) {
+				System.out.print("*");
+			}
+			System.out.println();			
+		}
+				
 	}
 
 }
