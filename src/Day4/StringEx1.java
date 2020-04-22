819af5801989da79022d4f9069d7938ffbbc363c
Gilson <dudrb8922@gmail.com>
2020-04-22 12:12:50 +0900

    4일차-String배열의 선언과 생성

---
+++ src/Day4/StringEx1.java
@@ -0,0 +1,19 @@
+package Day4;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		// String의 선언과 생성
+		String str="홍길동";
+		String str1=new String("임꺽정");
+		String arr[]=new String[3];
+		arr[0]=str;
+		arr[1]=str1;
+		arr[2]="이순신";
+		for(String s:arr) {
+			System.out.println(s);
+		}
+
+	}
+
+}
