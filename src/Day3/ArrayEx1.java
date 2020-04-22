9313154a8320453b63068b520cde349f2d0d5103
Gilson <dudrb8922@gmail.com>
2020-04-22 09:30:24 +0900

    3일차-배열의 선언과 생성

--- src/Day3/ArrayEx1.java
+++ src/Day3/ArrayEx1.java
@@ -17,8 +17,8 @@ public class ArrayEx1 {
 		n4=scan.nextInt();
 		n5=scan.nextInt();
 		*/
-		int []arr1, arr2; // 배열 선언, arr1과 arr2는 둘 다 배열
-		int arr3[], arr4; // arr3는 배열, arr4는 일반 변수
+		int []arr1, arr2; // 배열 선언, []뒤에 오는 모든 변수는 배열로 선언됨, arr1과 arr2는 둘 다 배열
+		int arr3[], arr4; // arr3는 배열, []앞의 하나의 변수만 배열로 선언, arr4는 일반 변수
 		arr1=new int[5]; // 배열 생성
 		int []arr5=new int[5]; // 배열 선언과 동시에 생성
 		arr1[0]=1;
