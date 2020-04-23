1e506ed4be4063b60f068d03fc6a01d55de29543
Gilson <dudrb8922@gmail.com>
2020-04-23 17:47:56 +0900

    5일차-클래스 예제

---
+++ src/Day5_1/DefaultClassEx1.java
@@ -0,0 +1,18 @@
+package Day5_1;
+
+public class DefaultClassEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class DefaultClassEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
