fe67c61557b726dffefef5ea6a3a8400d2f13d33
Gilson <dudrb8922@gmail.com>
2020-05-06 12:14:18 +0900

    10일차-추상클래스 예제

---
+++ src/Day10/AbstractEx1.java
@@ -0,0 +1,24 @@
+package Day10;
+
+public class AbstractEx1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+abstract class A{
+	int a;
+	public void print() {
+		System.out.println(a);
+	}
+	public abstract void test();
+}
+class B extends A{
+
+	@Override
+	public void test() {
+		
+	}
+	
+}
