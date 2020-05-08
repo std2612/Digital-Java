41315e6559a71b48c5dae0de2bf9a4010d0b271d
Gilson <dudrb8922@gmail.com>
2020-05-08 16:00:13 +0900

    5일차-클래스 및 접근 제한자 예제

--- src/Day5_1/DefaultClassEx1.java
+++ src/Day5_1/DefaultClassEx1.java
@@ -9,6 +9,7 @@ public class DefaultClassEx1 {
 		System.out.println("접근제한자 : public");
 		
 	}
+	
 }
 /* DefaultClassEx2 클래스는 접근제한자가 default이므로 같은 패키지나 같은
  * 클래스에서 불러올 수 있다. 다른 패키지에서는 DefaultClassEx2를 불러올 수 없다.
