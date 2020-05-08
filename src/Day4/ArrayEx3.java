238017dcacc59d886eaf18292ce57255e08179df
Gilson <dudrb8922@gmail.com>
2020-05-08 15:57:51 +0900

    4일차-배열을 이용한 향상된 for문 예제

--- src/Day4/ArrayEx3.java
+++ src/Day4/ArrayEx3.java
@@ -4,6 +4,7 @@ public class ArrayEx3 {
 
 	public static void main(String[] args) {
 		// 향상된 for문 예제
+		
 		int []arr=new int[] {1,3,5,7,2,4,6,8}; // 자동으로 갯수 지정됨 []안에 숫자 필요 없음
 		/* 향상된 for문을 주로 사용하는 경우
 		 * 1. 배열의 모든 값을 탐색하는 경우
