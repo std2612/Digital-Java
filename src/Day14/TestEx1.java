96c3124cead3a13dd540bca8598eb2ee7a5c06fb
Gilson <dudrb8922@gmail.com>
2020-05-12 09:43:00 +0900

    14일차-테스트 예제

--- src/Day14/TestEx1.java
+++ src/Day14/TestEx1.java
@@ -6,21 +6,37 @@ public class TestEx1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("제목을 입력하세요 : ");
-		String title=sc.nextLine();
-		System.out.print("단어을 입력하세요 : ");
-		String word=sc.next();
-		int idx = title.indexOf(word);
-		if(idx==-1) {
-		System.out.println(title+"에는 "+word+"라는 단어가 없습니다.");
+			while(true) {
+			System.out.print("제목을 입력하세요 : ");
+			String title=sc.nextLine();
+			if(title.equals("exit")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+			System.out.print("단어을 입력하세요 : ");
+			String word=sc.next();
 			
-		}
-		else {
-			System.out.println(title+"에는 "+word+"라는 단어가 있습니다.");
+			// indexOf
+			int idx = title.indexOf(word);
+			if(idx==-1) {
+				System.out.println(title+"에는 "+word+"라는 단어가 없습니다.");
+				
+			}
+			else {
+				System.out.println(title+"에는 "+word+"라는 단어가 있습니다.");
+			}
 			
+			// contains
+			boolean contain = title.contains(word);
+			if(!contain) {
+				System.out.println(title+"에는 "+word+"라는 단어가 없습니다.");
+			}
+			else {
+				System.out.println(title+"에는 "+word+"라는 단어가 있습니다.");
+			}
+			sc.nextLine();
 		}
 		sc.close();
-		
 		
 	}
 
