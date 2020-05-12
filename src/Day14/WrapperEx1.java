4baa4c53655806f0d0950c6b239279aa12e3d3d1
Gilson <dudrb8922@gmail.com>
2020-05-12 12:03:58 +0900

    14일차-래퍼 예제

---
+++ src/Day14/WrapperEx1.java
@@ -0,0 +1,19 @@
+package Day14;
+
+public class WrapperEx1 {
+
+	public static void main(String[] args) {
+		boolean b1 = true;
+		Boolean b2 = true;
+		char ch1 = 'a'; // ch1 변수에 a를 저장
+		Character ch2 = 'a'; // a가 저장된 주소를 ch2에 전달
+		ch1 = ch2;
+		int num1 = 10;
+		Integer num2 = 10;
+		double dnum1 = 1.23;
+		Double dnum2 = 1.23;
+		Double dnum3 = new Double(1.23);
+		
+	}
+
+}
