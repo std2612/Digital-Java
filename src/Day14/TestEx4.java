ca4afd6e75127c694c5d0772f5f5e20435bb448d
Gilson <dudrb8922@gmail.com>
2020-05-12 17:15:58 +0900

    14일차-메소드 예제

--- src/Day14/TestEx4.java
+++ src/Day14/TestEx4.java
@@ -3,20 +3,31 @@ package Day14;
 public class TestEx4 {
 
 	public static void main(String[] args) {
-		print.printMethod1("ㅋ", 10);
-		printMethod1("ㅋ", 10);
+		print1("ㅋ", 10);
+		System.out.println(print2('ㅋ', 10));
+		print3('ㅋ', 10);
+		
 	}
-	public static void printMethod1(String str, int num) {
-		for(int i=0; i<num; i++) {
-			System.out.printf(str);
+	public static void print1(String str, int cnt) {
+		for(int i=0; i<cnt; i++) {
+			System.out.print(str);
 		}
+		System.out.println();
+	}
+	public static String print2(char ch, int cnt) {
+		StringBuffer res = new StringBuffer(cnt);
+		for(int i=0; i<cnt; i++) {
+			res.append(ch);
+		}
+		return res.toString();
+	}
+	public static void print3(char ch, int cnt) {
+		StringBuffer res = new StringBuffer(cnt);
+		for(int i=0; i<cnt; i++) {
+			res.append(ch);
+		}
+		System.out.println(res.toString());
 	}
 	
 }
-class print {
-	public static void printMethod1(String str, int num) {
-		for(int i=0; i<num; i++) {
-			System.out.printf(str);
-		}
-	}
-}
+
