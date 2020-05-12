45c772046e8729678e47395c8fd226b1552b6598
Gilson <dudrb8922@gmail.com>
2020-05-12 09:25:14 +0900

    14일차-테스트 예제

--- src/Day14/TestEx1.java
+++ src/Day14/TestEx1.java
@@ -1,8 +1,8 @@
-package Day13;
+package Day14;
 
 import java.util.Scanner;
 
-public class Ex1 {
+public class TestEx1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
--- src/Day14/TestEx2.java
+++ src/Day14/TestEx2.java
@@ -1,8 +1,8 @@
-package Day13;
+package Day14;
 
 import java.util.Scanner;
 
-public class Ex2 {
+public class TestEx2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
