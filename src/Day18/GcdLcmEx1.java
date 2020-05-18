bdd969160f6a7f4025c34ed54f94865aa7c9adea
Gilson <dudrb8922@gmail.com>
2020-05-18 14:03:53 +0900

    18일차-최대 공약수 최소 공배수 예제

--- src/Day18/GcdLcmEx1.java
+++ src/Day18/GcdLcmEx1.java
@@ -18,36 +18,43 @@ public class GcdLcmEx1 {
 		sc.close();
 	}
 	
-	public static int gcd(int a, int b) {
+//	기능 : 두 정수의 최대 공약수를 알려주는 메서드
+//	매개변수 : 두 정수 => int num`, int num2
+//	리턴타입 : 최대 공약수 => int
+//	메소드명 : gcd
+	
+	public static int gcd(int num1, int num2) {
+		if(num1<0 || num2<0) {
+			throw new ArithmeticException("예외 : 최대 공약수는 두 양의 정수가 필요합니다.");
+		}
 		int gcd=1;
 		
-		if(a>b) {
-			int tmp=a;
-			a=b;
-			b=tmp;
+		if(num1>num2) {
+			int tmp=num1;
+			num1=num2;
+			num2=tmp;
 		}
 		
-		for(int i=1; i<=a; i++) {
-			if(a%i==0 && b%i==0) {
+		for(int i=num1; i>=1; i--) {
+			if(num1%i==0 && num2%i==0) {
 				gcd=i;
+				break;
 			}
 		}
-		
 		return gcd;
-		
 	}
 	
-	public static int lcm(int a, int b) {
+	public static int lcm(int num1, int num2) {
 		int lcm=1;
 		
-		if(a>b) {
-			int tmp=a;
-			a=b;
-			b=tmp;
+		if(num1>num2) {
+			int tmp=num1;
+			num1=num2;
+			num2=tmp;
 		}
 		
-		for(int i=1; i<=a*b; i++) {
-			if(i%a==0 && i%b==0) {
+		for(int i=1; i<=num1*num2; i++) {
+			if(i%num1==0 && i%num2==0) {
 				lcm=i;
 				break;
 			}
