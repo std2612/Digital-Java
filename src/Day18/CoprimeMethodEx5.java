281d87717b6fbc460e17dbb93bc09de38f2d66d5
Gilson <dudrb8922@gmail.com>
2020-05-18 17:35:55 +0900

    18일차-서로소 메소드 예제

--- src/Day18/CoprimeMethodEx5.java
+++ src/Day18/CoprimeMethodEx5.java
@@ -2,7 +2,7 @@ package Day18;
 
 import java.util.*;
 
-public class TestEx5 {
+public class CoprimeMethodEx5 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -13,7 +13,7 @@ public class TestEx5 {
 		num2=sc.nextInt();
 		
 		try {
-			if(isCoPrime(num1, num2)) {
+			if(isCoprime(num1, num2)) {
 				System.out.printf("%d, %d : 서로소입니다.", num1, num2);
 			}else {
 				System.out.printf("%d, %d : 서로소가 아닙니다.", num1, num2);
@@ -25,7 +25,7 @@ public class TestEx5 {
 		sc.close();
 	}
 	
-	public static boolean isCoPrime(int num1, int num2) {
+	public static boolean isCoprime(int num1, int num2) {
 		if(num1<0 || num2<0) {
 			throw new ArithmeticException("두 양의 정수가 필요합니다.");
 		}
@@ -35,18 +35,10 @@ public class TestEx5 {
 			num1=num2;
 			num2=tmp;
 		}
-		
-		int gcd=1;
-		
-		for(int i=1; i<=num1; i++) {
-			if(num1%i==0 && num2%i==0) {
-				gcd=i;
-			}
+		if(num1==num2) {
+			return false;
 		}
+		return GcdLcmMethodEx1.gcd(num1, num2)==1? true:false;
 		
-		if(gcd==1) {
-			return true;
-		}
-		return false;
 	}
 }
