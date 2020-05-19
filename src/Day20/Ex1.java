b11ff56f2026bac3f2af1187c5f4b07ddd94e0ad
Gilson <dudrb8922@gmail.com>
2020-05-19 17:35:25 +0900

    20일차-영어사전 예제

---
+++ src/Day20/Ex1.java
@@ -0,0 +1,27 @@
+package Day20;
+
+import java.util.*;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Set<String> dic=new HashSet<String>();
+		System.out.println("메뉴");
+		System.out.println("1. 단어 추가");
+		System.out.println("2. 단어에 뜻 추가");
+		System.out.println("3. 단어 삭제");
+		System.out.println("4. 뜻 삭제");
+		System.out.println("5. 단어 검색");
+		
+		
+	}
+
+}
+
+class Word {
+	String name;
+	Map<String, String> classMeaning= new HashMap<String, String>();
+	
+	
+	
+}
